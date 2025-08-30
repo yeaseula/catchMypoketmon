aaf6daa242873299a58c584ed216dae8353382ce
yeaseula <kminea@naver.com>
2025-08-30 22:11:54 +0900

    스크랩 페이지에 로컬데이터 호출 성공

--- my-next-poketmon/app/scrap/page.js
+++ my-next-poketmon/app/scrap/page.js
@@ -3,14 +3,25 @@
 import { useEffect, useState } from 'react';
 
 export default function ScrapPage() {
-
+    const [isLocal,setIsLocal] = useState(false)
+    const [localstate,setLocalstate] = useState([])
     useEffect(()=>{
-
-        //if(scraptarget.some ===)
+        const stateLocal = JSON.parse(localStorage.getItem("scrap") || "[]");
+        setLocalstate(stateLocal)
+        const States = stateLocal.length !== 0? true : false;
+        setIsLocal(States)
     },[])
     return (
         <div>
-
+            <div className={`is-not-cardlist ${isLocal? '' : 'on'}`}>
+                등록된 카드가 없어요
+            </div>
+            {localstate.map((ele,idx)=>(
+                <div className='slots' key={`${ele.id}-${idx}`}>
+                    <img src={ele.image}></img>
+                    <p>{ele.name}</p>
+                </div>
+            ))}
         </div>
     )
 }
