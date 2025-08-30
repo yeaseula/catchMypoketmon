fea1b85ba1a71c966ce3fb0d106cf65ac569d744
yeaseula <kminea@naver.com>
2025-08-30 22:25:09 +0900

    슬롯 갯수 모자랄 시 null값 추가

--- my-next-poketmon/app/scrap/page.js
+++ my-next-poketmon/app/scrap/page.js
@@ -1,8 +1,10 @@
 "use client";
 
 import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
 
 export default function ScrapPage() {
+    const [displayArr,setDisplayArr] = useState([])
     const [isLocal,setIsLocal] = useState(false)
     const [localstate,setLocalstate] = useState([])
     useEffect(()=>{
@@ -10,16 +12,32 @@ export default function ScrapPage() {
         setLocalstate(stateLocal)
         const States = stateLocal.length !== 0? true : false;
         setIsLocal(States)
+        const minSlots = 5;
+        setDisplayArr(prev => [...prev,...stateLocal])
+        while (displayArr.length < minSlots) {
+            displayArr.push(null)
+        }
     },[])
     return (
         <div>
             <div className={`is-not-cardlist ${isLocal? '' : 'on'}`}>
                 등록된 카드가 없어요
             </div>
-            {localstate.map((ele,idx)=>(
-                <div className='slots' key={`${ele.id}-${idx}`}>
-                    <img src={ele.image}></img>
-                    <p>{ele.name}</p>
+            {displayArr.map((ele,idx)=>(
+                <div className='slots slotWrapper' key={`scrap-${idx}`}>
+                    {ele ? (
+                        <div className="card box creative-pro">
+                            <img src={ele.image}></img>
+                            <p>{ele.id}</p>
+                            <p>{ele.name}</p>
+                            <p>{ele.types}</p>
+                        </div>
+                    ) : (
+                        <div className="card box creative-pro">
+                            <p>등록된 카드가 없습니다.</p>
+                        </div>
+                    )}
+
                 </div>
             ))}
         </div>
