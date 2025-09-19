4a1f9b5bc31c46be8065c7108d33481208c98e24
yeaseula <kminea@naver.com>
2025-09-20 01:33:01 +0900

    meta data 최상단으로 위치 변경

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,30 @@ import "../styles/main.css"
 import "../styles/sub.css"
 import Header from "./components/Header.js";
 
+export const metadata = {
+  title: "Catch My Pokemon!",
+  description: "나만의 포켓몬을 잡아라! 포켓몬 도감 수집 웹사이트",
+  icons: {
+    icon: [
+        { url: "/images/favicon.png", type: "image/png" },
+    ]
+  },
+  openGraph: {
+    title: "Catch My Pokemon!",
+    description: "나만의 포켓몬을 잡아라! 포켓몬 도감 수집 웹사이트",
+    url: "https://catch-mypoketmon.vercel.app/",
+    siteName: "Catch My Pokemon",
+    images: [
+      {
+        url: "/images/og-image.jpg",   // 대표 이미지
+        width: 1200,
+        height: 630
+      }
+    ],
+    locale: "ko_KR",
+    type: "website"
+  }
+};
 const myFont = LocalFont({
   src : [
     {path : '../public/fonts/Paperlogy-4Regular.woff2', weight : '400', style : 'normal'},
@@ -24,29 +48,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-export const metadata = {
-  title: "Catch My Pokemon!",
-  description: "나만의 포켓몬을 잡아라! 포켓몬 도감 수집 웹사이트",
-  icons: {
-    icon: "/images/favicon.png?v=2",
-  },
-  openGraph: {
-    title: "Catch My Pokemon!",
-    description: "나만의 포켓몬을 잡아라! 포켓몬 도감 수집 웹사이트",
-    url: "https://catch-mypoketmon.vercel.app/",
-    siteName: "Catch My Pokemon",
-    images: [
-      {
-        url: "/images/og-image.jpg",   // 대표 이미지
-        width: 1200,
-        height: 630
-      }
-    ],
-    locale: "ko_KR",
-    type: "website"
-  }
-};
 
 export default function RootLayout({ children }) {
   return (
