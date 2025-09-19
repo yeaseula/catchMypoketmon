d1773f0552afe14428694db1eeeb05c13c24c1ba
yeaseula <kminea@naver.com>
2025-09-19 20:24:17 +0900

    ⚡️ Detail SSR 방식으로 변경

---
+++ app/components/Loading.js
@@ -0,0 +1,10 @@
+export default function Loading() {
+    return (
+        <div className="loading-first">
+            <div className="loading-inner">
+                <img src='/images/main-loading.png' alt="로딩"></img>
+                <p>포켓몬 도감 생성중...</p>
+            </div>
+        </div>
+    )
+}
